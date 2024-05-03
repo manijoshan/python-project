7e6a84cca35676885b8fa18e3a910856a673b7a0
john alberto <john@gmail.com>
2024-05-03 08:35:30 +0000

    added

---
+++ setup.py
@@ -0,0 +1,14 @@
+rom setuptools import setup, find_packages
+
+setup(
+    name='studentXX_flask_app',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['main'],  # Explicitly include main.py
+    install_requires=[
+        'Flask>=3.0.3',
+        # Add other dependencies here
+    ],
+)
+
